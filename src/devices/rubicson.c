686a8ff0fb28e36e5f4e0aa8241377943ae2ab09
Marko Heinikangas <marhein@gmail.com>
2015-07-25 16:11:53 +0300

    add support for finding most propable datarow from 7/12 datarows for rubicson protocol. add support for humidity sensor to rubices device, copied from nexus.c add support for detecting sauna sensor on channel 4 and parse temperature separately from "normal" sensors on channels 1-3 wit

--- src/devices/rubicson.c
+++ src/devices/rubicson.c
@@ -6,6 +6,8 @@
  * the data is grouped into 9 nibles
  * [id0] [id1], [unk0] [temp0], [temp1] [temp2], [unk1] [unk2], [unk3]
  *
+ * sauna sensor sends only temperature on nibbles 5 6 7 (temp2, unk1, unk2)
+ *
  * The id changes when the battery is changed in the sensor.
  * unk0 is always 1 0 0 0, most likely 2 channel bits as the sensor can recevice 3 channels
  * unk1-3 changes and the meaning is unknown
@@ -17,26 +19,74 @@
 static int rubicson_callback(uint8_t bb[BITBUF_ROWS][BITBUF_COLS],int16_t bits_per_row[BITBUF_ROWS]) {
     int temperature_before_dec;
     int temperature_after_dec;
-    int16_t temp;
+    int16_t temp = 0;
+    int16_t humidity = 0;
+    int ch;
+    unsigned candidate_rows = 0;
+    int i,j;
+    uint8_t *datarow = NULL;
+    uint8_t *candidate_row = NULL;
+    unsigned candidate_val = 0;
+    for(i=1,candidate_rows=bits_per_row[0]>0;bits_per_row[i]>0;++i)
+        ++candidate_rows;
+    if(!(candidate_rows == 12||candidate_rows == 7))
+        return 0 ;
+    
+    if (bits_per_row[0]==36)
+        candidate_row = bb[0];
+    
+    for (i=1;bits_per_row[i]==36&&datarow==NULL;++i){
+        j=i;
+        if (!candidate_row)
+            candidate_row = bb[j++];
+        for (; bits_per_row[j]==36; ++j) {
+            if (memcmp(candidate_row,bb[j],5)==0)
+                ++candidate_val;
+        }
+        /* sauna sensor send 7 rows with last row missing last bit,
+         compare 4 bytes(no data in last byte anyway) */
+        if (candidate_rows == 7 && bits_per_row[0]==0 && bits_per_row[7] == 35)
+            if (memcmp(candidate_row,bb[7],4)==0)
+                ++candidate_val;
+        /* if found matches more than half of sent codes,
+         without counting candidate row, accepts as good signal*/
+        if(candidate_val++ > candidate_rows/2){
+            //printf("got candidate with %u/%u hits, accepting as datarow \n",candidate_val,candidate_rows);
+            datarow = candidate_row;
+        } else {
+            candidate_row=NULL;
+            candidate_val = 0;
+        }
+    }
 
     /* FIXME validate the received message better, figure out crc */
-    if (bb[1][0] == bb[2][0] && bb[2][0] == bb[3][0] && bb[3][0] == bb[4][0] &&
-        bb[4][0] == bb[5][0] && bb[5][0] == bb[6][0] && bb[6][0] == bb[7][0] && bb[7][0] == bb[8][0] &&
-        bb[8][0] == bb[9][0] && (bb[5][0] != 0 && bb[5][1] != 0 && bb[5][2] != 0)) {
-
-        /* Nible 3,4,5 contains 12 bits of temperature
-         * The temerature is signed and scaled by 10 */
-        temp = (int16_t)((uint16_t)(bb[0][1] << 12) | (bb[0][2] << 4));
-        temp = temp >> 4;
-
+    if (datarow[0]&&datarow[1]) {
+        /* channel number appears to last two bits of nibble 2,
+         increment by 1 to match switch numbering */
+        ch = ((datarow[1] &0x30)>>4) +1;
+        /* sauna sensor sends on channel 4 (1-3 selection-switch on other transmitters)*/
+        if (ch==4){
+            temp = datarow[3] | ((datarow[2] & 0xf)<<8);
+        }else {
+            /* Nible 3,4,5 contains 12 bits of temperature
+             * The temerature is signed and scaled by 10 */
+            temp = (int16_t)((uint16_t)(datarow[1] << 12) | (datarow[2] << 4));
+            temp = temp >> 4;
+            /* Nible 7,8 contain humidity value */
+            humidity = (int16_t)(((datarow[3]&0x0F)<<4)|(datarow[4]>>4));
+        }
+        
+        
         temperature_before_dec = abs(temp / 10);
         temperature_after_dec = abs(temp % 10);
 
         fprintf(stdout, "Sensor temperature event:\n");
         fprintf(stdout, "protocol       = Rubicson/Auriol, %d bits\n",bits_per_row[1]);
-        fprintf(stdout, "rid            = %x\n",bb[0][0]);
+        fprintf(stdout, "rid            = %x\n",datarow[0]);
         fprintf(stdout, "temp           = %s%d.%d\n",temp<0?"-":"",temperature_before_dec, temperature_after_dec);
-        fprintf(stdout, "%02x %02x %02x %02x %02x\n",bb[1][0],bb[0][1],bb[0][2],bb[0][3],bb[0][4]);
+        fprintf(stdout, "ch             = %u\n",ch);
+        fprintf(stdout, "humidity       = %d\n",humidity);
+        fprintf(stdout, "%02x %02x %02x %02x %02x\n",datarow[0],datarow[1],datarow[2],datarow[3],datarow[4]);
 
         if (debug_output)
             debug_callback(bb, bits_per_row);
